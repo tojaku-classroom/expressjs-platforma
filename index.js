ae5acb553c5444b5ecf53ad62510287e7bbe5c42
Tomislav Jakupić <tojaku@gmail.com>
2025-09-25 11:16:39 +0200

    Handlebars

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
 
 const app = express();
+
+app.engine('hbs', engine(
+    {
+        extname: '.hbs',
+        defaultLayout: 'main',
+        layoutsDir: path.join(__dirname, 'views', 'layouts'),
+        partialsDir: path.join(__dirname, 'views', 'partials')
+    }
+));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 const port = 3000;
 
 app.get('/', (request, response) => {
-    response.send('Bok!');
+    response.render('home', {name: 'Tomislav'});
 });
 
 app.listen(port, () => {
